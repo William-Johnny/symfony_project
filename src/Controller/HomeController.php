06f0251cfad446309332bf2e9e86577559f404cb
William-Johnny <wjguenon@icloud.com>
2026-02-19 10:57:20 +0100

    feat: creation form is now functional

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,36 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    // public function index(): Response
-    // {
-    //     $task = new Task();
-    //     return $this->render('home/index.html.twig', [
-    //         'controller_name' => 'HomeController',
-    //         'tasks' => [
-    //             [
-    //                 'name' => 'small thing to do',
-    //                 'description' => "Stuff to do like that and not like this cuz it's important to see this and not that",
-    //                 'level' => 'easy',
-    //                 'author' => 'Decima',
-    //                 'date' => new \DateTime('2023-02-11'),
-    //             ],
-    //             [
-    //                 'name' => 'hard thing to do',
-    //                 'description' => "Stuff to do like that and not like this cuz it's important to see this and not that",
-    //                 'level' => 'hard',
-    //                 'author' => 'Decima',
-    //                 'date' => new \DateTime('2023-02-11'),
-    //             ],
-    //             [
-    //                 'name' => $task->getName(),
-    //                 'description' => $task->getDescription(),
-    //                 'level' => $task->getLevel(),
-    //                 'author' => $task->getCreatedBy(),
-    //                 'date' => $task->getCreatedAt(),
-    //             ],
-    //         ],
-    //     ]);
-    // }
     public function show(EntityManagerInterface $entityManager): Response
     {
         $task = $entityManager
@@ -65,11 +35,9 @@ final class HomeController extends AbstractController
     #[Route('/entity/create', name: 'entity_create')]
     public function createTask(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $user = new User();
-        // $user->setUsername('John');
-        // $user->setEmail('124@yopmail.com');
-        // $user->setRoles(['tester']);
-        // $user->setPassword('tester');
+        $user = $entityManager
+            ->getRepository(User::class)
+            ->findOneBy(['email' => '126@yopmail.com']);
 
         $task = new Task();
         // $task->setName('thing to do');
@@ -83,9 +51,14 @@ final class HomeController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $task->setState('not done');
+            $task->setCreatedBy($user);
+            $task->setCreatedAt(new \DateTimeImmutable('2023-02-11'));
             $entityManager->persist($task);
             $entityManager->persist($user);
             $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('home/newTask.html.twig', [
